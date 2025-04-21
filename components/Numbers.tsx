077c7853a0122b13df6e1218c297072277f945e7
Thusithakit <thusitha@kongcepts.info>
2025-04-21 11:16:28 +0530

    added counter for numbers

--- components/Numbers.tsx
+++ components/Numbers.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AnimatedCounter from './AnimatedCounter'
 
 const Numbers = ({ number, slug, className }: { number: number, slug: string, className: string }) => {
     return (
         <div className={`justify-center items-start flex-col gap-2 ${className}`}>
-            <h3 className='text-[#323E59] text-[32px]'>{number}+</h3>
+            <h3 className='text-[#323E59] text-[32px]'><AnimatedCounter from={1} to={number} />+</h3>
             <p className='text-[#323E59] text-[18px]'>{slug}</p>
         </div>
     )
