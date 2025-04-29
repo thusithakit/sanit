451e7c20d4bd98b1105be60b2ad6c69d935a003b
Thusithakit <thusitha@kongcepts.info>
2025-04-29 11:48:43 +0530

    fixed accessibility tags order

--- components/Numbers.tsx
+++ components/Numbers.tsx
@@ -4,7 +4,7 @@ import AnimatedCounter from './AnimatedCounter'
 const Numbers = ({ number, slug, className }: { number: number, slug: string, className: string }) => {
     return (
         <div className={`justify-center items-start flex-col gap-2 ${className}`}>
-            <h3 className='text-[#323E59] text-[32px]'><AnimatedCounter from={1} to={number} />+</h3>
+            <span className='text-[#323E59] text-[32px] font-medium'><AnimatedCounter from={1} to={number} />+</span>
             <p className='text-[#323E59] text-[16px] lg:text-[18px]'>{slug}</p>
         </div>
     )
