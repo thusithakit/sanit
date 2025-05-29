24deb23451b95a77bd563b6a5173a55500b96bb9
Thusithakit <thusithakit3@gmail.com>
2025-05-29 14:00:05 +0530

    added icons for vision and mission

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import Card from '@/components/Card'
 import dynamic from 'next/dynamic'
+import { BsFillEyeFill,BsFillRocketTakeoffFill } from 'react-icons/bs';
 
 const TeamCarousel = dynamic(() => import('@/components/TeamCarousel'));
 
@@ -24,8 +25,8 @@ const page = () => {
                 <div className='lg:py-36 py-10 border border-x-0 border-t-0 border-b-[#E0EAFF]'>
                     <h1 className='lg:mb-20 mb-10'>Our <span>Vision</span> and <span>Mission</span></h1>
                     <div className="grid lg:grid-cols-2 lg:gap-8 gap-4">
-                        <Card className='bg-white border border-[#E5EDFF] pb-[41px]' cardTitle='Our Vision' cardContent='To provide businesses with seamless and secure networking and communication solutions, ensuring efficiency and reliability in an ever-evolving digital landscape.' />
-                        <Card className='bg-white border border-[#E5EDFF] pb-[41px]' cardTitle='Our Mission' cardContent='To be the leading IT solutions provider in Sri Lanka, revolutionizing business communication and security with cutting-edge technology.' />
+                        <Card className='bg-white border border-[#E5EDFF] pb-[41px]' cardTitle='Our Vision' icon={<BsFillEyeFill size={65}/>} cardContent='To provide businesses with seamless and secure networking and communication solutions, ensuring efficiency and reliability in an ever-evolving digital landscape.' />
+                        <Card className='bg-white border border-[#E5EDFF] pb-[41px]' cardTitle='Our Mission' icon={<BsFillRocketTakeoffFill size={65}/>} cardContent='To be the leading IT solutions provider in Sri Lanka, revolutionizing business communication and security with cutting-edge technology.' />
                     </div>
                 </div>
                 <div className='lg:py-44 py-10 border border-x-0 border-t-0 border-b-[#E0EAFF]'>
