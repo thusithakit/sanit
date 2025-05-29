d4478e3bd516879e529c5c62b94ccd76f28fc0f0
Thusithakit <thusithakit3@gmail.com>
2025-05-29 08:35:13 +0530

    fixed clients page mobile view

--- components/ClientDetailsCard.tsx
+++ components/ClientDetailsCard.tsx
@@ -9,17 +9,17 @@ interface ClientCardProps {
 const ClientDetailsCard = ({clientDetails,index}:ClientCardProps) => {
 const zeroPad = (num:number) => String(num).padStart(2, '0')
   return (
-    <div className={`flex flex-wrap mx-auto w-fit ${index % 2 === 0? 'lg:flex-row-reverse':''}`}>
+    <div className={`flex flex-wrap mx-auto w-fit mb-10 lg:mb-0 ${index % 2 === 0? 'lg:flex-row-reverse':''}`}>
       <Image src={clientDetails.img} alt={clientDetails.title} width={544} height={600} className='object-scale-down'/>
       <div className={`max-w-[544px] flex flex-col justify-center ${index %2 === 0?'lg:pr-[55px]':'lg:pl-[55px]'}`}>
         <h1 className='text-[#92ABD6] opacity-30 lg:mb-6 mb-3'>{zeroPad(index)}</h1>
-        <h2 className='lg:text-[40px] text-[30px] text-[#92ABD6] lg:mb-[25px] mb-[15px] leading-none'>{clientDetails.title}</h2>
-        <p className='text-[#748099] lg:text-[18px] text-[16px] lg:mb-8 mb-2'>{clientDetails.description}</p>
-        <div className='grid lg:grid-cols-2 gap-4'>
+        <h2 className='lg:text-[40px] text-[26px] text-[#92ABD6] lg:mb-[25px] mb-[15px] leading-none'>{clientDetails.title}</h2>
+        <p className='text-[#748099] lg:text-[18px] text-[14px] lg:mb-8 mb-2'>{clientDetails.description}</p>
+        <div className='grid grid-cols-2 gap-4'>
             {clientDetails.points.map((point,i)=>(
                 <div key={i} className='flex items-start gap-3 text-[#323E59]'>
                     <Image src="/clients/points-icon.svg" alt='check icon' width={20} height={20}/>
-                    <p>{point}</p>
+                    <p className='lg:text-[18px] text-[14px]'>{point}</p>
                 </div>
             ))}
         </div>
