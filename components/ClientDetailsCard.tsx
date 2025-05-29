1dba27051fb74835252faba1f6db6ced01dcfdeb
Thusithakit <thusithakit3@gmail.com>
2025-05-29 13:55:41 +0530

    change points gap on mobile

--- components/ClientDetailsCard.tsx
+++ components/ClientDetailsCard.tsx
@@ -15,7 +15,7 @@ const zeroPad = (num:number) => String(num).padStart(2, '0')
         <h1 className='text-[#92ABD6] opacity-30 lg:mb-3 mb-1 number'>{zeroPad(index)}</h1>
         <h2 className='lg:text-[40px] text-[26px] text-[#92ABD6] lg:mb-[20px] mb-[10px] leading-none'>{clientDetails.title}</h2>
         <p className='text-[#748099] lg:text-[18px] text-[14px] lg:mb-8 mb-2'>{clientDetails.description}</p>
-        <div className='grid grid-cols-2 gap-4'>
+        <div className='grid lg:grid-cols-2 gap-2.5 lg:gap-4'>
             {clientDetails.points.map((point,i)=>(
                 <div key={i} className='flex items-start gap-3 text-[#323E59]'>
                     <Image src="/clients/points-icon.svg" alt='check icon' width={20} height={20}/>
